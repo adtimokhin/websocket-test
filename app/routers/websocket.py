b2682f7418a920b986cc88a731278cf70f806bfa
Aleksandr Timokhin <adtimokhin@gmail.com>
2025-07-17 10:18:25 +0200

    added a simple websocket echo functionality

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -4,16 +4,24 @@ Main place for routers.
 Contains the websocket endpoinds only
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 websocket_router = APIRouter()
 
 # Endpoinds
 
 @websocket_router.websocket("/echo")
-async def echo_chat():
-    pass
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
 
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"{data}")
+
+    except WebSocketDisconnect:
+        print("Connection closed by client")
+        return
 # Test
 @websocket_router.get("/")
 async def test():
